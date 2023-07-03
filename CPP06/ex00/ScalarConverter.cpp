49b9f4348c0c0544f3efa8a1c2d64c6f5f66992b
Benjamin Chabot <bchabot@3F5.42angouleme.fr>
2023-07-03 14:06:18 +0200

    start CPP07

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,15 +6,14 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 15:30:29 by bchabot           #+#    #+#             */
-/*   Updated: 2023/06/30 15:47:44 by bchabot          ###   ########.fr       */
+/*   Updated: 2023/07/03 14:05:59 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <stdlib.h>
 #include <iostream>
-#include <limits.h>
-#include <cmath>
+#include <climits>
 
 ScalarConverter::ScalarConverter(void) {
 	return ;
@@ -48,57 +47,57 @@ void printNan(void) {
 	std::cout << "double: nan" << std::endl;
 }
 
-void printChar(const char &c) {
-	if (c >= 32 && c <= 126)
-		std::cout << "char: '" << c << "'" << std::endl;
+void printChar(const char &c, const double &d) {
+	if (d >= 32 && d <= 126)
+		std::cout << "char: '" << static_cast<char>(c) << "'" << std::endl;
 	else
 		std::cout << "char: non displayable" << std::endl;
 }
 
 void printInt(const int &nbr, const double &d) {
-	if (d <= INT_MAX)
-		std::cout << "int: " << static_cast<int>(nbr) << std::endl;
+	if (d <= INT_MAX && d >= INT_MIN)
+		std::cout << "int: " << nbr << std::endl;
 	else
 		std::cout << "int: overflow" << std::endl;
 }
 
 void printFloat(const float &nbr) {
 	if (nbr >= 1000000 || (nbr - (int)nbr != 0))
-		std::cout << "float: " << static_cast<float>(nbr) << "f" << std::endl;
+		std::cout << "float: " << nbr << "f" << std::endl;
 	else
-		std::cout << "float: " << static_cast<float>(nbr) << ".0f" << std::endl;
+		std::cout << "float: " << nbr << ".0f" << std::endl;
 }
 
 void printDouble(const double &nbr) {
 	if (nbr >= 1000000 || (nbr - (int)nbr != 0))
-		std::cout << "double: " << static_cast<double>(nbr) << std::endl;
+		std::cout << "double: " << nbr << std::endl;
 	else
-		std::cout << "double: " << static_cast<double>(nbr) << ".0" << std::endl;
+		std::cout << "double: " << nbr << ".0" << std::endl;
 }
 
 void convertInt(std::string to_convert, const double &d) {
 	int nbr = atoi(to_convert.c_str());
 	
-	printChar(nbr);
+	printChar(static_cast<char>(nbr), d);
 	printInt(nbr, d);
-	printFloat(static_cast<float>(nbr));
-	printDouble(static_cast<double>(nbr));
+	printFloat(static_cast<float>(d));
+	printDouble(static_cast<double>(d));
 }
 
 void convertFloat(std::string to_convert, const double &d) {
 	float nbr = strtof(to_convert.c_str(), NULL);
 
-	printChar(static_cast<char>(nbr));
+	printChar(static_cast<char>(nbr), d);
 	printInt(static_cast<int>(nbr), d);
-	printFloat(nbr);
-	printDouble(static_cast<double>(nbr));
+	printFloat(d);
+	printDouble(static_cast<double>(d));
 }
 
 void convertDouble(std::string to_convert) {
 	double nbr = strtod(to_convert.c_str(), NULL);
 
 	if (!isInf(to_convert))
-		printChar(nbr);
+		printChar(nbr, nbr);
 	else
 		std::cout << "char: impossible" << std::endl;
 	if (!isInf(to_convert))
@@ -116,7 +115,7 @@ void identify(const std::string &to_convert) {
 		printNan();
 		return ;
 	}
-	if (to_convert.find(".") != std::string::npos && *ptr == 'f' && !isInf(to_convert))
+	if (to_convert.find(".") != std::string::npos && (*ptr == 'f' && *(ptr + 1) == '\0') && !isInf(to_convert))
 		convertFloat(to_convert, d);
 	else if (to_convert.find(".") == std::string::npos && *ptr == '\0' && !isInf(to_convert))
 		convertInt(to_convert, d);
@@ -127,6 +126,10 @@ void identify(const std::string &to_convert) {
 }
 
 void ScalarConverter::convert(const std::string &to_convert) {
+	if (to_convert.empty()) {
+		std::cout << "Impossible conversion, check your argument." << std::endl;
+		return ;
+	}
 	if (to_convert.length() == 1 && !isdigit(to_convert[0])) {
 		std::cout << "char: " << to_convert[0] << std::endl;
 		std::cout << "int: " << static_cast<int>(to_convert[0]) << std::endl;
