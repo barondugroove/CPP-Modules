4fd2fced6cdd092d5507d61299bec1605310ed83
Benjamin Chabot <bchabot@1E3.42angouleme.fr>
2023-07-13 18:05:17 +0200

    CPP08 Finished

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 12:13:36 by bchabot           #+#    #+#             */
-/*   Updated: 2023/07/12 12:16:13 by bchabot          ###   ########.fr       */
+/*   Updated: 2023/07/13 16:16:20 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 # define MUTANTSTACK_HPP
 
 #include <stack>
+#include <deque>
+#include <iterator>
 
 template<typename T>
 class MutantStack : public std::stack<T> {
+	public :
+		typedef typename std::deque<T>::iterator iterator;
 
+		iterator begin() {
+			return this->c.begin();
+		}
+		
+		iterator end() {
+			return this->c.end();
+		}
 };
 
 #endif
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -6,32 +6,133 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 12:16:33 by bchabot           #+#    #+#             */
-/*   Updated: 2023/07/12 12:16:48 by bchabot          ###   ########.fr       */
+/*   Updated: 2023/07/13 18:04:50 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+#include <iostream>
+#include <stack>
+#include <list>
+#include <vector>
+
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << "_____ TESTS WITH MUTANT STACK _____" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "mstack top is : ";
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "mstack size is : ";
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
-	return 0;
+	{
+		std::cout << "_____ TESTS WITH LIST _____" << std::endl;
+		std::list<int> mslist;
+		mslist.push_front(5);
+		mslist.push_front(17);
+		std::cout << "mstack list is : ";
+		std::cout << mslist.front() << std::endl;
+		mslist.pop_front();
+		std::cout << "mstack size is : ";
+		std::cout << mslist.size() << std::endl;
+		mslist.push_back(3);
+		mslist.push_back(5);
+		mslist.push_back(737);
+		//[...]
+		mslist.push_back(0);
+		std::list<int>::iterator it = mslist.begin();
+		std::list<int>::iterator ite = mslist.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(std::list<int>);
+	}
+	{
+		std::cout << "_____ TESTS WITH SUBIELO _____" << std::endl;
+		std::vector<std::pair<int, int> > msvector;
+		msvector.insert(msvector.begin(), std::pair<int, int>(5, 5));
+		msvector.insert(msvector.begin(), std::pair<int, int>(17, 17));
+		std::cout << "msvector vector<pair> is : ";
+		std::cout << msvector.begin()->first << std::endl;
+		msvector.erase(msvector.begin());
+		std::cout << "msvector vector<pair> size is : ";
+		std::cout << msvector.size() << std::endl;
+		msvector.push_back(std::pair<int, int>(3, 3));
+		msvector.push_back(std::pair<int, int>(5, 5));
+		msvector.push_back(std::pair<int, int>(737, 737));
+		//[...]
+		msvector.push_back(std::pair<int, int>(0, 0));
+		std::vector<std::pair<int, int> >::iterator it = msvector.begin();
+		std::vector<std::pair<int, int> >::iterator ite = msvector.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << it->first << " | " << it->second <<  std::endl;
+			++it;
+		}
+		std::stack<int> s(std::vector<std::pair<int, int> >);
+	}
+	{
+		std::cout << "_____ TESTS WITH SUBIELO 2 _____" << std::endl;
+		MutantStack<std::pair<int, MutantStack<int> > > mstack;
+		MutantStack<int> tmp;
+		tmp.push(5);
+		mstack.push(std::pair<int, MutantStack<int> >(1, tmp));
+		tmp.push(17);
+		mstack.push(std::pair<int, MutantStack<int> >(2, tmp));
+		std::cout << "mstack top is : ";
+		std::cout << mstack.begin()->first << " | " << *mstack.begin()->second.begin() << std::endl;
+		tmp.pop();
+		std::cout << "mstack size is : ";
+		std::cout << mstack.size() << std::endl;
+		tmp.push(3);
+		mstack.push(std::pair<int, MutantStack<int> >(3, tmp));
+		tmp.push(5);
+		mstack.push(std::pair<int, MutantStack<int> >(4, tmp));
+		tmp.push(737);
+		mstack.push(std::pair<int, MutantStack<int> >(5, tmp));
+		//[...]
+		mstack.push(std::pair<int, MutantStack<int> >(6, tmp));
+		MutantStack<std::pair<int, MutantStack<int> > >::iterator it = mstack.begin();
+		MutantStack<std::pair<int, MutantStack<int> > >::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			MutantStack<int>::iterator lol = it->second.begin();
+			std::cout << it->first << " | ";
+			while (lol != it->second.end())
+			{	
+				std::cout << *lol << " ";
+				lol++;
+			}
+			std::cout << std::endl;
+			++it;
+		}
+		std::stack<int> s(MutantStack<std::pair<int, MutantStack<int> > >);
+	}
 }
