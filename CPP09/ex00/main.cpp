e4ca34695ae66b32579450f12a96cded25fa050b
Benjamin Chabot <bchabot@1B5.42angouleme.fr>
2023-07-17 17:53:37 +0200

    Need points de correction :'( for CPP08

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -6,53 +6,42 @@
 /*   By: bchabot <bchabot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 15:52:16 by bchabot           #+#    #+#             */
-/*   Updated: 2023/07/16 19:40:12 by bchabot          ###   ########.fr       */
+/*   Updated: 2023/07/17 17:52:30 by bchabot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include <iostream>
+#include <vector>
 #include <map>
 #include <fstream>
 #include <sstream>
 
-std::string copyFile(char *fileName) {
-	std::string buf;
-	std::ifstream file;
-	std::stringstream str;
-
-	file.open(fileName);
-	buf = "";
-	if (!file.is_open())
-		return (buf);
-	str << file.rdbuf();
-	buf = str.str();
-	if (buf.empty())
-		return (buf);
-	file.close();
-	return (buf);
-}
-
 void createDb(std::vector<std::pair<std::string, int> > db, std::string input) {
-	std::string date;
+	std::string tmp;
+	std::ifstream file;
 
 	(void)db;
-	input.erase(0, input.find("\n"));
-	date = input.substr(0, input.find(" "));
-	std::cout << date << std::endl;
+	//input.erase(0, input.find("\n"));
+	tmp = "";
+	file.open(input.c_str());
+	std::getline(file, tmp, '\n' );
+	std::cout << tmp << std::endl;
 }
 
 int main(int ac, char **av)
 {
 	std::string buf;
 	std::filebuf input;
-	
-	if (ac != 2) {
+
+	(void)av;	
+	(void)ac;
+/*	if (ac != 2) {
 		std::cout << "Wrong number of arguments." << std::endl;
 		return 1;
-	}
+	}*/
 	std::vector<std::pair<std::string, int> > db;
-	buf = copyFile(av[1]);
-	std::cout << buf << std::endl;
-	createDb(db, buf);
+	std::map<std::string, int> dbBitcoin;
+	createDb(db, "data.csv");
 	return 0;
 }
